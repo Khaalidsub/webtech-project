77b6ac8d5d65b128a6bc8a9d9ed55f463320a32b
Khaalid <khaalidsubaan@gmail.com>
2021-07-10 20:17:44 +0800

    update db

--- webtechbackend/test/db.php
+++ webtechbackend/test/db.php
@@ -4,14 +4,14 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
 
     class db{
         // Properties
-        private $host = 'localhost';
+        private $host = '165.232.170.162';
         private $user = 'root';
         private $password = 'webtech123';
         private $dbname = 'webtechbackenddb';
 
         // Connect
         public function connect(){
-            $mysql_connect_str = "mysql:host=$this->host;dbname=$this->dbname;";
+            $mysql_connect_str = "mysql:host=$this->host;dbname=$this->dbname;port=3306;charset=utf8";
             $dbConnection = new PDO($mysql_connect_str, $this->user, $this->password);
             $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $dbConnection;
